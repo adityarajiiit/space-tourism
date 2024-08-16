c75294cb198451294eed9e570d5128a2ac895541
adityarajiiit <148451902+adityarajiiit@users.noreply.github.com>
2024-08-16 20:55:34 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import {
+    withHydrationOverlay,
+  } from "@builder.io/react-hydration-overlay/next";
+  
+const nextConfig = {
+    experimental:{
+serverActions:true,
+serverComponentsExternalPackages:["mongoose"]
+    },
+    images:{
+        remotePatterns:[
+            {
+                protocol:'https',
+                hostname:'img.clerk.com'
+            },
+            {
+                protocol:"https",
+                hostname:"images.clerk.dev"
+            },
+            {
+                protocol:"https",
+                hostname:"uploadthing.com"
+            },
+            {
+                protocol:"https",
+                hostname:'placehold.co'
+            },
+        ],
+        
+    }
+};
 
-export default nextConfig;
+export default withHydrationOverlay({
+    /**
+     * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
+     * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
+     */
+    appRootSelector: "main",
+  })(nextConfig);
